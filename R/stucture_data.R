288eb00642ac791b9f3d24fdbe1a02fc0089531e
Thomas Husken <thomashusken@gmail.com>
2017-02-20 16:18:00 +0100

    Add the structure_data function

--- R/stucture_data.R
+++ R/stucture_data.R
@@ -1,8 +1,19 @@
+#' Structure recurrent events data to 3d format
+#'
+#' \code{structure_data} restructures a wide-format matrix to a 3d array, which can be passed on to \code{fitrem} to fit a model
+#'
+#' @param data a wide-format matrix
+#' @param dcols column names containing capture dates
+#' @param xcolnames column names containing time-invariant covariates
+#' @param otime the observation window in days
+#' @return a 3D-array of dimension n x otime x \code{length(xcolnames)}
+#' @export
+
 structure_data <- function(data, dcols, xcolnames, otime, ...) {
     n <- dim(data)[1]
     events <- make_event_matrix(data, dcols, ...)
     covmatrix <- make_cov_array(data, xcolnames, otime, ...)
-    data <- abind(events, covmatrix)
+    data <- abind::abind(events, covmatrix)
     output <- data
     dimnames(output)[[3]] <- c("C", paste0(dimnames(covmatrix)[[3]]))
     return(output)
@@ -15,33 +26,33 @@ make_event_matrix <- function(data, dcols, startt, sel, ...) {
     }
 
     if (class(data[, dcols[1]]) %in% c("POSIXct", "Date")) {
-        DaysOfYear <- apply(data[, dcols], 2, function(x) ymd(x) - startt)
+        days_of_year <- apply(data[, dcols], 2, function(x) ymd(x) - startt)
     } else if (class(data[, dcols[1]]) %in% c("numeric", "integer")) {
-        DaysOfYear <- data[, dcols]
+        days_of_year <- data[, dcols]
     }
 
     n <- dim(data)[1]
 
     if (missing(sel)) {
         otime <- max(ymd(data[, dcols[1]])) - min(ymd(data[, dcols[1]]))
-        Events <- matrix(0, n, otime)
+        events <- matrix(0, n, otime)
         for (i in 1:n) {
-            Events[i, as.numeric(DaysOfYear[i, ])] <- 1
+            events[i, as.numeric(days_of_year[i, ])] <- 1
         }
     } else {
         if (class(sel) == "Date") {
             num <- (ymd(sel) + 1) - startt
             Events <- matrix(0, n, (max(num) + 1) - min(num))
-            for (j in 1:dim(DaysOfYear)[2]) {
-                pick <- apply(DaysOfYear, 2, function(x) which(x %in% num))[[j]]
+            for (j in 1:dim(days_of_year)[2]) {
+                pick <- apply(days_of_year, 2, function(x) which(x %in% num))[[j]]
                 for (i in pick) {
-                  Events[i, as.numeric(DaysOfYear[i, j]) - (min(num - 1))] <- 1
+                  Events[i, as.numeric(days_of_year[i, j]) - (min(num - 1))] <- 1
                 }
             }
         } else if (class(sel) != "Date")
             stop("sel should be of class Date")
     }
-    output <- Events
+    output <- events
 }
 
 make_cov_array <- function(data, xcolnames, otime, ...) {
@@ -50,7 +61,7 @@ make_cov_array <- function(data, xcolnames, otime, ...) {
     n <- dim(covs)[1]
     k <- dim(covs)[2]
     covmatrix <- array(0, dim = c(n, k, otime))
-    dimnames(CovMatrix) <- list(NULL, c(dimnames(covs)[[2]]), paste0("d", 1:otime))
+    dimnames(covmatrix) <- list(NULL, c(dimnames(covs)[[2]]), paste0("d", 1:otime))
     covmatrix[, , ] <- covs
     covmatrix <- aperm(covmatrix, perm = c(1, 3, 2))
 }
